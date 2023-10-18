4c5af425423b827d072a36581560573f47e8b70f
Malik Kotb <malikkotb@icloud.com>
2023-10-18 19:29:13 +0200

    layout mat's portfolio site

---
+++ app/page.js
@@ -0,0 +1,22 @@
+import Model from "../components/Model";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col justify-between h-screen">
+      <div className="flex justify-between h-1/4">
+        <div>Home About</div>
+        <div className="">Matteo Just</div>
+      </div>
+
+      <div className="h-2/4 grid grid-cols-4">
+        <Model source={"source"} />
+        <Model source={"source"} />
+        <Model source={"source"} />
+        <Model source={"source"} />
+
+      </div>
+
+      <div className="h-1/4">Description</div>
+    </main>
+  );
+}
---
+++ components/Model.js
@@ -0,0 +1,6 @@
+
+export default function Model({ source }) {
+  return (
+    <div className="flex justify-center items-center bg-cyan-600 border "></div>
+  )
+}
