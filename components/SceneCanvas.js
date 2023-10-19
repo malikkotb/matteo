359141d5dcf1754b30adcaf5f65f55412c560ddf
Malik Kotb <malikkotb@icloud.com>
2023-10-19 11:35:10 +0200

    using orbitcontrols instead of presnetationcontrols for better viewing

--- components/SceneCanvas.js
+++ components/SceneCanvas.js
@@ -1,24 +1,40 @@
 "use client";
 
-import { useRef } from "react";
+import { Suspense, useRef } from "react";
 import { Canvas, useFrame, useLoader } from "@react-three/fiber";
-import { OrbitControls, PresentationControls, Stage, useGLTF } from "@react-three/drei";
-import { Mesh } from "three";
+import {
+  OrbitControls,
+  PresentationControls,
+  Stage,
+  useGLTF,
+} from "@react-three/drei";
 import Model from "./Model";
 
-
 export default function SceneCanvas({ source }) {
+  return (
+    <div className="flex justify-center items-center bg-cyan-600">
+      {/* <Canvas className="">
+            <OrbitControls enableZoom={false} />
+            <ambientLight intensity={0.5} />
+            <directionalLight position={[-2, 5, 2]} intensity={1} />
+            <Suspense fallback={null}>
+                <Model source={source} />
+            </Suspense>
+        </Canvas> */}
 
-    return (
-    <div className="flex justify-center items-center bg-cyan-600 border">
-        <Canvas dpr={[1,2]} shadows camera={{ fov: 45 }} >
-            <color attach="background" args={["#101010"]} />
-            <PresentationControls speed={1.5} zoom={0.5} polar={[-0.1, Math.PI / 4]}>
-                <Stage environment={null}>
-                    <Model source={source} scale={0.01} />
-                </Stage>
-            </PresentationControls>
-        </Canvas>
+      <Canvas dpr={[1, 2]} shadows camera={{ fov: 45 }}>
+        <color attach="background" />
+        <OrbitControls
+          autoRotate
+          zoom0={0.5}
+          enableZoom={false}
+          minPolarAngle={-Math.PI / 2}
+          maxPolarAngle={Math.PI / 2}
+        / >
+          <Stage environment={null}>
+            <Model source={source} scale={0.01} />
+          </Stage>
+      </Canvas>
     </div>
-  )
+  );
 }
