ca2b21ec0fd7d1611270c91c0b4d9ee2da438f35
Malik Kotb <malikkotb@icloud.com>
2023-10-20 11:50:22 +0200

    split screen scrolltrigger pin

--- app/mini/page.js
+++ app/mini/page.js
@@ -1,72 +1,75 @@
 "use client";
 import SceneCanvas from "../../components/SceneCanvas";
 import gsap from "gsap";
-import React, { useState, useEffect, useRef } from "react";
-import styles from "./styles.module.css";
+import React, { useRef, useLayoutEffect } from "react";
 
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export default function page() {
-  useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
-    ScrollTrigger.create({
-      trigger: ".gallery",
-      start: "top top",
-      end: "bottom bottom",
-      pin: ".right",
+  const imageContainer = useRef(null);
+  const pinContainerRef = useRef(null);
+  gsap.registerPlugin(ScrollTrigger);
+
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      ScrollTrigger.create({
+        trigger: imageContainer.current,
+        start: "top top",
+        end: "bottom bottom",
+        // markers: true,
+        pin: pinContainerRef.current,
+      });
     });
+
+    return () => ctx.revert();
   }, []);
-    
+
   return (
     <div>
-      <div className={styles.spacer}></div>
-      {/* <div className="w-1/2 overflow-hidden">
-        <SceneCanvas source={"/mini.glb"} />
-      </div> */}
+      <div className="w-screen h-[50vh] bg-black grid place-content-center">
+        <div className=" text-4xl text-white">Mini 2023</div>
+      </div>
 
-      <div className={styles.gallery}>
-        <div className={styles.left}>
-          <div className={styles.detailsWrapper}>
-            <div className={styles.details}>
-              <div className={styles.headline}></div>
-              <div className={styles.text}></div>
-              <div className={styles.text}></div>
-              <div className={styles.text}></div>
-              <div className={styles.text}></div>
+      <div ref={imageContainer} className={`flex outline outline-1 outline-red-600`}>
+        <div className="left w-[50%]">
+          <div className="m-auto w-[80%]">
+            <div className="h-screen flex flex-col justify-center outline outline-1 outline-red-700">
+              <div className="w-full text-3xl">The New Mini - 2022</div>
+              <div>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptas et, similique sequi odio ipsam obcaecati assumenda
+                corporis ex pariatur quos totam? Fugiat eum veniam iusto a
+                praesentium voluptatum enim autem!
+              </div>
+            </div>
+            <div className="h-screen flex flex-col justify-center outline outline-1 outline-red-700">
+              <div className="w-full text-3xl">The New Mini - 2022</div>
+              <div>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptas et, similique sequi odio ipsam obcaecati assumenda
+                corporis ex pariatur quos totam? Fugiat eum veniam iusto a
+                praesentium voluptatum enim autem!
+              </div>
             </div>
 
-            <div className={styles.details}>
-              <div className={styles.headline}></div>
-              <div className={styles.text}></div>
-              <div className={styles.text}></div>
-              <div className={styles.text}></div>
-              <div className={styles.text}></div>
-            </div>
-
-            <div className={styles.details}>
-              <div className={styles.headline}></div>
-              <div className={styles.text}></div>
-              <div className={styles.text}></div>
-              <div className={styles.text}></div>
-              <div className={styles.text}></div>
+            <div className="h-screen flex flex-col justify-center outline outline-1 outline-red-700">
+              <div className="w-full text-3xl">The New Mini - 2022</div>
+              <div>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptas et, similique sequi odio ipsam obcaecati assumenda
+                corporis ex pariatur quos totam? Fugiat eum veniam iusto a
+                praesentium voluptatum enim autem!
+              </div>
             </div>
           </div>
         </div>
 
-        <div className={styles.right}>
-          <div className={styles.photos}></div>
+        <div ref={pinContainerRef} className={`h-screen outline outline-1 outline-red-700 flex flex-col justify-center`}>
+          <div className="rounded-xl w-[40vw] h-[40vw] bg-blue-500"></div>
         </div>
       </div>
-      <div className={styles.spacer}></div>
-      <div className={styles.spacer}></div>
-      <div className={styles.spacer}></div>
 
+      <div className="w-screen h-[50vh] bg-black grid place-content-center"></div>
     </div>
   );
-}
-
-{
-  /* <div className="spacer"></div>
-      <div className="spacer"></div>
-      <div className="spacer"></div> */
 }
